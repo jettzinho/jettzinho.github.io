4c4c1bceed057b7524c1f5b8eb9fb7b360ed380c
jettzinho <161106103+jettzinho@users.noreply.github.com>
2024-02-24 02:24:01 -0300

    upload do site xcam

---
+++ ddawdadwdwdawdawdwdawdawww/ddawdadwdwdawdawdwdawdawww/config.js
@@ -0,0 +1,45 @@
+module.exports = {
+    server: {
+        ativado: false,
+        id: '1046122581654241370',
+        convite: 'https://discord.gg/'
+    },
+    pagina: {
+        particulas: 'https://cdn.discordapp.com/emojis/998607318893723798.png',
+        avatar_redondo: true,
+        background: '',
+        cor_fundo: '#000001',
+        titulo_pagina: 'xcam é muito bom véi!'
+        },
+    bot: {
+        token: 'TOKEN DE UM BOT DO DISCORD.'
+    },
+    // ID dos usuários que vão aparecer no website.
+    usuarios: {
+      '1053875008839090286': 'mes_9',
+      '785656584155824129': 'mes_18',
+      '294877324829261824': null,
+    },
+    badges: {
+        nitro: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/discordnitro.svg',
+        PremiumEarlySupporter: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/discordearlysupporter.svg',
+        ActiveDeveloper: 'https://raw.githubusercontent.com/efeeozc/discord-badges/6c54af4b871af1e6daef9722f02257af18446129/svg/activedeveloper.svg',
+        HypeSquadOnlineHouse1: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/hypesquadbravery.svg',
+        HypeSquadOnlineHouse2: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/hypesquadbrilliance.svg',
+        HypeSquadOnlineHouse3: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/hypesquadbalance.svg',
+        VerifiedDeveloper: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/discordbotdev.svg',
+        novo_nick: 'https://github.com/mezotv/discord-badges/blob/main/assets/username.png?raw=true'
+    },
+    boost: {
+        mes_1: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost1.svg',
+        mes_2: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost2.svg',
+        mes_3: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost3.svg',
+        mes_6: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost4.svg',
+        mes_9: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost5.svg',
+        mes_12: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost6.svg',
+        mes_15: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost7.svg',
+        mes_18: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost8.svg',
+        mes_24: 'https://raw.githubusercontent.com/mezotv/discord-badges/90629519c9f7e44096613a1e83c0a554cae49046/assets/boosts/discordboost9.svg'
+    },
+    
+};
---
+++ ddawdadwdwdawdawdwdawdawww/ddawdadwdwdawdawdwdawdawww/squarecloud.app
@@ -0,0 +1,8 @@
+MAIN=index.js
+MEMORY=528
+VERSION=recommended
+DISPLAY_NAME=xcam!
+DESCRIPTION=xcam.
+SUBDOMAIN=xcam
+START=npm run start
+AUTORESTART=true
